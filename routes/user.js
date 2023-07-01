f3b66233f9ecf9086bd0ff1965726f6323e9ce89
Fajar Putra <106464666+fajarph@users.noreply.github.com>
2023-07-01 15:56:31 +0700

    test

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,9 @@ const { Router } = require('express');
 const controllers = require('../controllers/user.js');
 const router = Router()
 
+router.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 router.get('/users', controllers.getUsers)
 router.get('/users/:id', controllers.getUserById)
 router.post('/users', controllers.createUser)

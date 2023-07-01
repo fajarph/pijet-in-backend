7b18ce0d8fb75a7e143ab62a66fb6919da50b699
Fajar Putra <106464666+fajarph@users.noreply.github.com>
2023-07-01 14:09:58 +0700

    create controllers order and routes

---
+++ controllers/order.js
@@ -0,0 +1,33 @@
+const { Order } = require("../models")
+
+const getOrders = async(req, res) => {
+    try {
+        const response = await Order.findAll({
+            attributes: ["nama", "gender", "durasiLayanan","layananTambahan"]
+        });
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(500).json({msg: error.message})
+    }
+}
+
+const createOrder = async(req, res) => {
+    const {nama, gender, durasiLayanan, layananTambahan} = req.body;
+    try {
+        await Order.create({
+            nama: nama,
+            gender: gender,
+            durasiLayanan: durasiLayanan,
+            layananTambahan: layananTambahan,
+            UserId: req.userId
+        })
+        res.status(201).json({msg: "Order Berhasil Terbuat"})
+    } catch (error) {
+        res.status(400).json({msg: error.message})
+    }
+}
+
+module.exports = {
+    getOrders,
+    createOrder
+}
